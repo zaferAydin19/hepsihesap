11d14711a8ec0fbe9c94764b7ecd577f850e071f
Zafer Aydın <zaferaydin93@outlook.com>
2025-07-20 15:21:21 +0300

    lk Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hepsihesap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
